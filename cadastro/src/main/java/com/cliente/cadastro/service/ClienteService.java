2cb9b0e2f6e9c591e09a17502f36b1cf3f8e8d7b
Solmayer Alves <solmayer.alves@gmail.com>
2024-05-28 16:36:08 -0300

    Camada Service (ClienteService) criada com lista de consulta e consulta por id de clientes.

---
+++ cadastro/src/main/java/com/cliente/cadastro/service/ClienteService.java
@@ -0,0 +1,38 @@
+package com.cliente.cadastro.service;
+
+import com.cliente.cadastro.entity.Cliente;
+import com.cliente.cadastro.repository.ClienteRepository;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@Entity
+@Builder
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente salvarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> consultarCliente() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> consultarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+}
