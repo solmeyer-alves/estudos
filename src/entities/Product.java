61ea2d244ec8fd34114281f2eeb63e1bf5ed952f
Solmayer Alves <solmayer.alves@gmail.com>
2025-10-08 11:19:06 -0300

    metodo estatico na classe Product

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -26,6 +26,9 @@ public class Product {
 		this.price = price;
 	}
 	
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >= 100.0;
+	}
 	
 	@Override
 	public String toString() {
