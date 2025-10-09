fbb42d6dbf226281a79bb4b6c232702827235828
Solmayer Alves <solmayer.alves@gmail.com>
2025-10-09 16:55:28 -0300

    expressao lambda inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,10 +23,7 @@ public class Program {
 		 /* Pode-se utilizar uma variável para informar o valor */
 		 double min = 100.0;
 		
-		 /* Expressao lambda declarada */
-		Predicate<Product> prod = p -> p.getPrice() >= min; 
-		
-		list.removeIf(prod);
+		list.removeIf(p -> p.getPrice() >= min);
 		
 		for(Product p : list) {
 			System.out.println(p);
