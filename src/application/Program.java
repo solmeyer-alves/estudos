4bb629549256d328d92dba412d01a8044e63f038
Solmayer Alves <solmayer.alves@gmail.com>
2025-10-11 13:14:41 -0300

    implementando Function, stream e map

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.Uppercasename;
 
 public class Program {
 
@@ -20,13 +21,9 @@ public class Program {
 		 list.add(new Product("Tablet", 350.50));
 		 list.add(new Product("HD Case", 80.90));
 		 
-		double factor = 1.1;
-
-		list.forEach(p -> p.setPrice(p.getPrice() * factor));
-		
-		list.forEach(System.out::println);
+		 List<String> names =  list.stream().map(new Uppercasename()).collect(Collectors.toList());
 		 
-		 
+		 names.forEach(System.out::println);
 	}
 
 }
