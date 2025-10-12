a5cb92ec5bab8516ee8eb19294d82b91f06c349c
Solmayer Alves <solmayer.alves@gmail.com>
2025-10-12 16:07:58 -0300

    metodo nao estatico

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.Uppercasename;
 
 public class Program {
 
@@ -21,7 +21,8 @@ public class Program {
 		 list.add(new Product("Tablet", 350.50));
 		 list.add(new Product("HD Case", 80.90));
 		 
-		 List<String> names =  list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+		 
+		 List<String> names =  list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
 		 
 		 names.forEach(System.out::println);
 	}
