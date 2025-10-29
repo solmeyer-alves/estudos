4ad1b4a11436feb23d849222fa274990202dd45e
Solmayer Alves <solmayer.alves@gmail.com>
2025-10-29 11:12:14 -0300

    Creating project and Git repository

--- cadastro/src/main/java/com/cliente/cadastro/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cliente.cadastro.repository;
-
-import com.cliente.cadastro.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module dao_project {
+	requires java.sql;
+}
